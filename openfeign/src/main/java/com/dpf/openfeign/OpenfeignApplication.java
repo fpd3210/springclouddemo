964d821bc5ba1b38778f951ce5070e98b36214ce
pikachues <446933040@qq.com>
2020-04-01 16:02:50 +0800

    添加OpenFeign

---
+++ openfeign/src/main/java/com/dpf/openfeign/OpenfeignApplication.java
@@ -0,0 +1,21 @@
+package com.dpf.openfeign;
+
+import feign.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableFeignClients
+public class OpenfeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenfeignApplication.class, args);
+    }
+
+    @Bean
+    Logger.Level loggerLevel(){
+        return Logger.Level.FULL;
+    }
+}
---
+++ openfeign/src/test/java/com/dpf/openfeign/OpenfeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpf.openfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
