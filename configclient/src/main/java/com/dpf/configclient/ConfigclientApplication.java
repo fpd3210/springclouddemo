3fa0cb8912fe384456a1e9923dc16d6b58b5f294
pikachues <446933040@qq.com>
2020-05-17 16:31:27 +0800

    添加Spring Cloud Config

---
+++ configclient/src/main/java/com/dpf/configclient/ConfigclientApplication.java
@@ -0,0 +1,13 @@
+package com.dpf.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigclientApplication.class, args);
+    }
+
+}
